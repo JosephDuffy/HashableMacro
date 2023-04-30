089e47cb1040873a7874d6c38ccafb929b529b39
Joseph Duffy <commits@josephduffy.co.uk>
2023-04-30 10:09:26 +0100

    Remove unused dependencies

--- Package.swift
+++ Package.swift
@@ -26,9 +26,6 @@ let package = Package(
       dependencies: [
         .product(name: "SwiftSyntax", package: "swift-syntax"),
         .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
-        .product(name: "SwiftOperators", package: "swift-syntax"),
-        .product(name: "SwiftParser", package: "swift-syntax"),
-        .product(name: "SwiftParserDiagnostics", package: "swift-syntax"),
         .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
       ]
     ),
