8f329d34a102961d89dd5d5f25631e6f0a79527e
Joseph Duffy <commits@josephduffy.co.uk>
2023-04-30 11:28:09 +0100

    Remove TODO; this matches `MyOptionSet` from examples

--- Sources/CustomHashable/Macros.swift
+++ Sources/CustomHashable/Macros.swift
@@ -1,6 +1,5 @@
 import Foundation
 
-// TODO: Check what names value should be here
 @attached(member, names: arbitrary)
 @attached(conformance)
 public macro CustomHashable() = #externalMacro(module: "CustomHashablePlugin", type: "CustomHashable")
