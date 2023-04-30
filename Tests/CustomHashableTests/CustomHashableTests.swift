541f0792bf3d181e5f2d7249e4ba4f57346579a4
Joseph Duffy <commits@josephduffy.co.uk>
2023-04-30 11:31:57 +0100

    Move type outside function, add extra tests
    When the type is declared in the function it causes a compilation error.
    
    These tests show that the synthesised conformance is used.

--- Tests/CustomHashableTests/CustomHashableTests.swift
+++ Tests/CustomHashableTests/CustomHashableTests.swift
@@ -1,28 +1,66 @@
 import XCTest
 import CustomHashable
 
+struct NotHashableType {}
+
+@CustomHashable
+struct CustomHashableStructWithMultipleProperties: Hashable {
+    @HashableKey
+    let firstProperty: Int
+
+    @HashableKey
+    let secondProperty: Int
+
+    @HashableKey
+    private let thirdProperty: Int
+
+    init(firstProperty: Int, secondProperty: Int, thirdProperty: Int) {
+        self.firstProperty = firstProperty
+            self.secondProperty = secondProperty
+self.thirdProperty = thirdProperty
+    }
+}
+
+@CustomHashable
+struct CustomHashableStructWithExcludedProperty: Hashable {
+    @HashableKey
+    let firstProperty: Int
+
+    @HashableKey
+    let secondProperty: Int
+
+    let excludedProperty: Int
+
+    init(firstProperty: Int, secondProperty: Int, excludedProperty: Int) {
+        self.firstProperty = firstProperty
+            self.secondProperty = secondProperty
+self.excludedProperty = excludedProperty
+    }
+}
+
 final class CustomHashableTests: XCTestCase {
     func testCustomHashableStructMultipleProperties() {
-        @CustomHashable
-        struct CustomHashableType: Hashable {
-            @HashableKey
-            let firstProperty: Int
-
-            @HashableKey
-            let secondProperty: Int
-
-            @HashableKey
-            let thirdProperty: Int
-        }
-
-        let value1 = CustomHashableType(firstProperty: 1, secondProperty: 2, thirdProperty: 3)
-        let value2 = CustomHashableType(firstProperty: 2, secondProperty: 2, thirdProperty: 3)
-        let value3 = CustomHashableType(firstProperty: 3, secondProperty: 3, thirdProperty: 4)
+        let value1 = CustomHashableStructWithMultipleProperties(firstProperty: 1, secondProperty: 2, thirdProperty: 3)
+        let value2 = CustomHashableStructWithMultipleProperties(firstProperty: 2, secondProperty: 2, thirdProperty: 3)
+        let value3 = CustomHashableStructWithMultipleProperties(firstProperty: 1, secondProperty: 2, thirdProperty: 4)
 
         XCTAssertEqual(value1, value1)
         XCTAssertEqual(value2, value2)
         XCTAssertEqual(value3, value3)
         XCTAssertNotEqual(value1, value3)
+        XCTAssertNotEqual(value2, value3)
+        XCTAssertNotEqual(value1, value2)
+    }
+
+    func testCustomHashableStructWithExcludedProperty() {
+        let value1 = CustomHashableStructWithExcludedProperty(firstProperty: 1, secondProperty: 2, excludedProperty: 3)
+        let value2 = CustomHashableStructWithExcludedProperty(firstProperty: 2, secondProperty: 2, excludedProperty: 3)
+        let value3 = CustomHashableStructWithExcludedProperty(firstProperty: 1, secondProperty: 2, excludedProperty: 4)
+
+        XCTAssertEqual(value1, value1)
+        XCTAssertEqual(value2, value2)
+        XCTAssertEqual(value3, value3)
+        XCTAssertEqual(value1, value3, "Third property should not be included in equality check; synthesised conformance should not be used")
         XCTAssertNotEqual(value2, value3)
         XCTAssertNotEqual(value1, value2)
     }
