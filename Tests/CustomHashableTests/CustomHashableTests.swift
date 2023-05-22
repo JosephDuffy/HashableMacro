25c6ea926f76d0c06b47bba42ee8f4c6d10c9c2b
Joseph Duffy <commits@josephduffy.co.uk>
2023-05-22 19:41:32 +0100

    Move types used in tests to tests target

--- Tests/CustomHashableTests/CustomHashableTests.swift
+++ Tests/CustomHashableTests/CustomHashableTests.swift
@@ -58,3 +58,38 @@ final class CustomHashableTests: XCTestCase {
         XCTAssertNotEqual(value1, value2)
     }
 }
+
+@CustomHashable
+private struct CustomHashableStructWithExcludedProperty {
+    @HashableKey
+    let firstProperty: Int
+
+    @HashableKey
+    private let secondProperty: Int
+
+    let excludedProperty: Int
+
+    init(firstProperty: Int, secondProperty: Int, excludedProperty: Int) {
+        self.firstProperty = firstProperty
+            self.secondProperty = secondProperty
+self.excludedProperty = excludedProperty
+    }
+}
+
+@CustomHashable
+public class CustomHashableClassWithPrivateProperty {
+    @HashableKey
+    let firstProperty: Int
+
+    @HashableKey
+    let secondProperty: Int
+
+    @HashableKey
+    private let privateProperty: Int
+
+    init(firstProperty: Int, secondProperty: Int, privateProperty: Int) {
+        self.firstProperty = firstProperty
+        self.secondProperty = secondProperty
+        self.privateProperty = privateProperty
+    }
+}
