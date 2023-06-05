adf2eb759a8fa0c1e4e906e43576ef317701c94e
Joseph Duffy <commits@josephduffy.co.uk>
2023-06-05 23:26:43 +0100

    Use swift-syntax snapshot used in template in Xcode

--- Package.swift
+++ Package.swift
@@ -17,13 +17,14 @@ let package = Package(
   dependencies: [
     .package(
       url: "https://github.com/apple/swift-syntax.git",
-      branch: "main"
+      from: "509.0.0-swift-5.9-DEVELOPMENT-SNAPSHOT-2023-04-25-b"
     ),
   ],
   targets: [
     .macro(
       name: "CustomHashablePlugin",
       dependencies: [
+        .product(name: "SwiftDiagnostics", package: "swift-syntax"),
         .product(name: "SwiftSyntax", package: "swift-syntax"),
         .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
         .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
