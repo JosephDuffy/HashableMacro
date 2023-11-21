f84a500910b7facf8130de47fe64c864d8aa6e61
Joseph Duffy <commits@josephduffy.co.uk>
2023-11-20 18:20:57 -0800

    Add more tests

--- Tests/CustomHashableTests/CustomHashableTests.swift
+++ Tests/CustomHashableTests/CustomHashableTests.swift
@@ -42,21 +42,18 @@ final class CustomHashableTests: XCTestCase {
         let value2 = CustomHashableClassWithPrivateProperty(firstProperty: 2, secondProperty: 2, privateProperty: 3)
         let value3 = CustomHashableClassWithPrivateProperty(firstProperty: 1, secondProperty: 2, privateProperty: 4)
 
-        var hasher = Hasher()
-        var hasher2 = hasher
-        value1.hash(into: &hasher)
-        1.hash(into: &hasher2)
-        2.hash(into: &hasher2)
-        3.hash(into: &hasher2)
-
-        XCTAssertEqual(hasher.finalize(), hasher2.finalize())
-
         XCTAssertEqual(value1, value1)
+        XCTAssertEqual(value1.hashValue, value1.hashValue)
         XCTAssertEqual(value2, value2)
+        XCTAssertEqual(value2.hashValue, value2.hashValue)
         XCTAssertEqual(value3, value3)
+        XCTAssertEqual(value3.hashValue, value3.hashValue)
         XCTAssertNotEqual(value1, value3)
+        XCTAssertNotEqual(value1.hashValue, value3.hashValue)
         XCTAssertNotEqual(value2, value3)
+        XCTAssertNotEqual(value2.hashValue, value3.hashValue)
         XCTAssertNotEqual(value1, value2)
+        XCTAssertNotEqual(value1.hashValue, value2.hashValue)
     }
 
     func testTypeNotExplicitlyConformingToHashable() {
@@ -95,6 +92,194 @@ final class CustomHashableTests: XCTestCase {
                     return lhs.hashablePropery1 == rhs.hashablePropery1
                         && lhs.hashablePropery2 == rhs.hashablePropery2
                         && lhs.hashablePropery3 == rhs.hashablePropery3
+                }
+            }
+            """,
+            macros: testMacros
+        )
+    }
+
+    func testTypeWithoutAnyHashableKeys() {
+        assertMacroExpansion(
+            """
+            @CustomHashable
+            struct TypeWithoutHashableKeys {
+                var notHashedProperty: String
+            }
+            """,
+            expandedSource: """
+
+            struct TypeWithoutHashableKeys {
+                var notHashedProperty: String
+
+                func hash(into hasher: inout Hasher) {
+
+                }
+
+                static func == (lhs: TypeWithoutHashableKeys, rhs: TypeWithoutHashableKeys) -> Bool {
+                    return true
+                }
+            }
+            """,
+            macros: testMacros
+        )
+    }
+
+    func testTypeWithExplicitHashableConformation() {
+        assertMacroExpansion(
+            """
+            @CustomHashable
+            struct TypeWithExplicitHashableConformation: Hashable {
+                @HashableKey
+                var hashedProperty: String
+            }
+            """,
+            expandedSource: """
+
+            struct TypeWithExplicitHashableConformation: Hashable {
+                var hashedProperty: String
+
+                func hash(into hasher: inout Hasher) {
+                    hasher.combine(self.hashedProperty)
+                }
+
+                static func == (lhs: TypeWithExplicitHashableConformation, rhs: TypeWithExplicitHashableConformation) -> Bool {
+                    return lhs.hashedProperty == rhs.hashedProperty
+                }
+            }
+            """,
+            macros: testMacros
+        )
+    }
+
+    func testPublicType() {
+        assertMacroExpansion(
+            """
+            @CustomHashable
+            public struct PublicType {
+                @HashableKey
+                var hashedProperty: String
+            }
+            """,
+            expandedSource: """
+
+            public struct PublicType {
+                var hashedProperty: String
+
+                public func hash(into hasher: inout Hasher) {
+                    hasher.combine(self.hashedProperty)
+                }
+
+                public static func == (lhs: PublicType, rhs: PublicType) -> Bool {
+                    return lhs.hashedProperty == rhs.hashedProperty
+                }
+            }
+            """,
+            macros: testMacros
+        )
+    }
+
+    func testExplicitlyInternalType() {
+        assertMacroExpansion(
+            """
+            @CustomHashable
+            internal struct ExplicitlyInternalType {
+                @HashableKey
+                var hashedProperty: String
+            }
+            """,
+            expandedSource: """
+
+            internal struct ExplicitlyInternalType {
+                var hashedProperty: String
+
+                internal func hash(into hasher: inout Hasher) {
+                    hasher.combine(self.hashedProperty)
+                }
+
+                internal static func == (lhs: ExplicitlyInternalType, rhs: ExplicitlyInternalType) -> Bool {
+                    return lhs.hashedProperty == rhs.hashedProperty
+                }
+            }
+            """,
+            macros: testMacros
+        )
+    }
+
+    func testFilePrivateType() {
+        assertMacroExpansion(
+            """
+            @CustomHashable
+            fileprivate struct FilePrivateType {
+                @HashableKey
+                var hashedProperty: String
+            }
+            """,
+            expandedSource: """
+
+            fileprivate struct FilePrivateType {
+                var hashedProperty: String
+
+                fileprivate func hash(into hasher: inout Hasher) {
+                    hasher.combine(self.hashedProperty)
+                }
+
+                fileprivate static func == (lhs: FilePrivateType, rhs: FilePrivateType) -> Bool {
+                    return lhs.hashedProperty == rhs.hashedProperty
+                }
+            }
+            """,
+            macros: testMacros
+        )
+    }
+
+    func testPrivateType() {
+        assertMacroExpansion(
+            """
+            @CustomHashable
+            private struct PrivateType {
+                @HashableKey
+                var hashedProperty: String
+            }
+            """,
+            expandedSource: """
+
+            private struct PrivateType {
+                var hashedProperty: String
+
+                func hash(into hasher: inout Hasher) {
+                    hasher.combine(self.hashedProperty)
+                }
+
+                static func == (lhs: PrivateType, rhs: PrivateType) -> Bool {
+                    return lhs.hashedProperty == rhs.hashedProperty
+                }
+            }
+            """,
+            macros: testMacros
+        )
+    }
+
+    func testPublicFinalType() {
+        assertMacroExpansion(
+            """
+            @CustomHashable
+            public final class PublicFinalType {
+                @HashableKey
+                var hashedProperty: String
+            }
+            """,
+            expandedSource: """
+
+            public final class PublicFinalType {
+                var hashedProperty: String
+
+                public func hash(into hasher: inout Hasher) {
+                    hasher.combine(self.hashedProperty)
+                }
+
+                public static func == (lhs: PublicFinalType, rhs: PublicFinalType) -> Bool {
+                    return lhs.hashedProperty == rhs.hashedProperty
                 }
             }
             """,
@@ -160,4 +345,26 @@ struct TypeWithMultipleVariablesOnSameLine {
     let hashablePropery3, hashablePropery4: String
 
     var notHashablePropery: String
+}
+
+@CustomHashable
+fileprivate struct FilePrivateType {
+    @HashableKey
+    var hashablePropery1: String
+}
+
+@CustomHashable
+private struct PrivateType {
+    @HashableKey
+    var hashedProperty: String
+}
+
+@CustomHashable
+public final class PublicFinalType {
+    @HashableKey
+    var hashedProperty: String
+
+    init(hashedProperty: String) {
+        self.hashedProperty = hashedProperty
+    }
 }
