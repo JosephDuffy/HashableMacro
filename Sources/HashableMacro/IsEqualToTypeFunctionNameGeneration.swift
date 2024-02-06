496817883b5b19390ba188117ffb56f581242f09
Joseph Duffy <commits@josephduffy.co.uk>
2024-02-05 22:25:31 -0800

    Only import HashableMacroFoundation when ObjectiveC can be imported

--- Sources/HashableMacro/IsEqualToTypeFunctionNameGeneration.swift
+++ Sources/HashableMacro/IsEqualToTypeFunctionNameGeneration.swift
@@ -1,3 +1,8 @@
+#if canImport(HashableMacroFoundation)
+#if canImport(ObjectiveC)
 import HashableMacroFoundation
 
 public typealias IsEqualToTypeFunctionNameGeneration = HashableMacroFoundation.IsEqualToTypeFunctionNameGeneration
+#else
+#warning("ObjectiveC should be importable when HashableMacroFoundation can be imported")
+#endif
