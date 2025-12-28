7c53e21ac5ebe9f37419fa610a9c4c34ef1a3dbd
Joseph Duffy <commits@josephduffy.co.uk>
2025-12-28 10:22:43 -0800

    Generate test matrix

---
+++ .github/workflows/generate-xcode-tests-matrix.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+set -e
+
+platforms='["macOS","iOS","tvOS","watchOS","macCatalyst","visionOS"]'
+
+includes='[
+  {"os":"macos-14","xcode":"15.1"},
+  {"os":"macos-14","xcode":"15.3"},
+  {"os":"macos-15","xcode":"16.0"},
+  {"os":"macos-15","xcode":"16.3"},
+  {"os":"macos-26","xcode":"26.0.1"}
+]'
+
+jq --null-input \
+  --argjson platforms "$platforms" \
+  --argjson includes "$includes" \
+  '
+  reduce $includes[] as $inc (
+    [];
+    . + [
+      $platforms[]
+      | {
+          platform: .,
+          os: $inc.os,
+          xcode: $inc.xcode
+        }
+      | select(.platform != "visionOS" or .xcode != "15.1")
+    ]
+  )
+  '
