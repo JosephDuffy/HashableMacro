8001e3f0fa9e870f49f906976e99ff9f0903b083
Joseph Duffy <commits@josephduffy.co.uk>
2025-12-28 15:06:15 -0800

    Use Xcode 15.2 to enable visionOS support

--- .github/generate-xcode-tests-matrix.sh
+++ .github/generate-xcode-tests-matrix.sh
@@ -5,11 +5,11 @@ set -e
 platforms='["macOS","iOS","tvOS","watchOS","macCatalyst","visionOS"]'
 
 includes='[
-  {"os":"macos-14","xcode":"15.1"},
-  {"os":"macos-14","xcode":"15.3"},
-  {"os":"macos-15","xcode":"16.0"},
-  {"os":"macos-15","xcode":"16.3"},
-  {"os":"macos-26","xcode":"26.0.1"}
+  {"os":"macos-14", "xcode":"15.2", "swift": "5.9"},
+  {"os":"macos-14", "xcode":"15.3", "swift": "5.10"},
+  {"os":"macos-15", "xcode":"16.0", "swift": "6.0"},
+  {"os":"macos-15", "xcode":"16.3", "swift": "6.1"},
+  {"os":"macos-26", "xcode":"26.0.1", "swift": "6.2"}
 ]'
 
 jq --null-input \
